1ad458d9242cd344b84eee4e5e50d7a96c99f6e7
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-06-09 16:58:40 +0530

    UI Changes

--- app/page.js
+++ app/page.js
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="w-full bg-black text-[white] flex items-center justify-center h-5/6">
-      <div className="flex flex-col gap-4 justify-center items-center px-2">
+      <div className="flex flex-col gap-4 justify-center items-center px-2 mb-20">
         <div className="space-y-4 text-center">
           <h1 className="text-4xl font-bold tracking-tight sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
             Effortlessly Manage Your Inbox
           </h1>
-          <p className="max-w-[600px] mx-auto text-lightGray md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+          <p className="max-w-[600px] mx-auto text-[#FAFAFA] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
             Classify, filter, and organize your emails with our powerful email
             management app. Streamline your workflow and stay on top of your
             inbox.
--- components/EmailPreview.jsx
+++ components/EmailPreview.jsx
@@ -26,14 +26,14 @@ const EmailPreview = ({
   return (
     <div
       onClick={() => handleClick()}
-      className={`border border-gray rounded-md p-4 cursor-pointer hover:bg-gray transition-all space-y-2 relative ${
+      className={`border border-gray rounded-xl p-5 cursor-pointer hover:bg-gray transition-all space-y-2 relative ${
         selectedMailID === id ? "bg-gray" : "bg-black"
       }`}
     >
       <div className="text-2xl font-semibold">{fromName}</div>
       <div className="w-4/5 font-medium">Sub: {subject}</div>
       <div className="text-textGray text-wrap">
-        <pre className="text-wrap break-words whitespace-pre-wrap">
+        <pre className="text-wrap break-words whitespace-pre-wrap text-sm tracking-tight">
           {snippet}
         </pre>
       </div>
